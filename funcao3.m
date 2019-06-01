6a6b659b0c27d4348519ecbf1665004c309b0134
GabrielZMN <gabriel.zmn@poli.ufrj.br>
2019-06-01 17:17:16 -0300

    Update funcao3.m

--- funcao3.m
+++ funcao3.m
@@ -1,10 +1,11 @@
 den = [1 8 16];
-num =[4 24];
+%%para alfa = 1/4
+num =[6 24];
 %%definindo funcao de transferencia
 sys = tf(num,den);
 xlabel('Seg(s)');
 ylabel('Amplitude');
-%%criando gráfico da função de transferência a respota ao degrau unitario
+%%criando grÃ¡fico da funÃ§Ã£o de transferÃªncia a respota ao degrau unitario
 step(sys);
 %%definindo polos, ganho e zeros
 [z,p,k]=tf2zp(num,den);
