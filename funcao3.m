07b1afd2db0415555e4436f277721e60ae6d86af
Brenno Rodrigues de Carvalho da Silva <brennorcs@poli.ufrj.br>
2019-06-02 22:11:27 -0300

    Update funcao3.m

--- funcao3.m
+++ funcao3.m
@@ -1,5 +1,4 @@
 den = [1 8 16];
-%%para alfa = 1/4
 num =[6 24];
 %%definindo funcao de transferencia
 sys = tf(num,den);
@@ -7,7 +6,26 @@ xlabel('Seg(s)');
 ylabel('Amplitude');
 %%criando gráfico da função de transferência a respota ao degrau unitario
 step(sys);
-%%definindo polos, ganho e zeros
+%definindo polos, ganho e zeros
 [z,p,k]=tf2zp(num,den);
 pzmap(sys);
 [Wn,a] = damp(sys);
+bode(sys)
+%w = 2pi
+[u,t] = gensig('sin',1,5,0.01);
+lsim(sys,u,t)
+%w = 4pi
+[u,t] = gensig('sin',0.5,2.5,0.01);
+lsim(sys,u,t)
+
+%w = 8pi
+[u,t] = gensig('sin',0.25,2.5,0.01);
+lsim(sys,u,t)
+
+%w = 16pi
+[u,t] = gensig('sin',0.125,2.5,0.01);
+lsim(sys,u,t)
+
+%w = 32pi
+[u,t] = gensig('sin',0.0625,2.5,0.01);
+lsim(sys,u,t)
